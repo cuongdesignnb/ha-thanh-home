b2e229bb40c3593b1c295cf764b5a16783fab53e
cuongdesign <dinhcuongdesign@gmail.com>
2026-08-24 10:33:25 +0700

    fix(web): recover recent GSC 404 URLs

--- apps/web/src/lib/rich-content.spec.ts
+++ apps/web/src/lib/rich-content.spec.ts
@@ -26,7 +26,28 @@ assert.equal(getLegacyRedirectTarget("/xay-nha-tron-goi-quan-hai-ba-trung"), "/d
 assert.equal(getLegacyRedirectTarget("/bao-gia-xay-nha-tai-quan-hoan-kiem"), "/du-an/xay-nha-tron-goi-phuong-hoan-kiem-hathanhhome");
 assert.equal(getLegacyRedirectTarget("/bao-gia-xay-dung-nha-tron-goi-gia-re"), "/du-an/bao-gia-xay-nha-tron-goi-tai-ha-noi-2026");
 assert.equal(getLegacyRedirectTarget("/noi-that-phong-ngu"), "/mau-thiet-ke-noi-that/mau-phong-ngu-dep-am-cung-va-hien-dai");
+const gscRedirects: Array<[string, string]> = [
+  ["/du-an/xay-nha-tron-goi-tai-xa-an-khanh-ha-noi-hathanhhome", "/du-an/xay-nha-tron-goi-tai-xa-an-khanh-ha-noi"],
+  ["/mau-thiet-ke-kien-truc/35-mau-biet-thu-tan-co-dien-san-vuon-dep-nha-2026", "/mau-thiet-ke-kien-truc/35-mau-biet-thu-tan-co-dien-san-vuon-dep-2026"],
+  ["/du-an/xay-nha-tron-goi-tai-ha-dong-khao-sat-tu-van-mien-phi", "/du-an/xay-nha-tron-goi-tai-ha-dong"],
+  ["/du-an/cong-trinh-nha-o-biet-thu-3-tang-tai-bac-ninh-dep-hien-dai", "/du-an/cong-trinh-nha-o-biet-thu"],
+  ["/cong-ty-noi-that-van-phong-mien-bac-uy-tin-chuyen-nghiep", "/du-an/cong-ty-noi-that-van-phong-mien-bac"],
+  ["/xay-nha-tron-goi-ba-vi-ho-tro-24/7-cong-ty-xay-dung-uy-tin", "/du-an/xay-nha-tron-goi-ba-vi-ho-tro-20-7-cong-ty-xay-dung-uy-tin"],
+  ["/70%20-mau-biet-thu-tan-co-dien-hien-dai-dep-nha-2026", "/mau-thiet-ke-kien-truc/70-mau-biet-thu-tan-co-dien-hien-dai-dep-2026"],
+  ["/du-an/xay-nha-tron-goi-long-bien-ha-noi", "/du-an/xay-nha-tron-goi-long-bien-ha-noi-hathanhhome"],
+  ["/du-an/xay-nha-tron-goi-tai-ha-dong-khao-sat-tu-van-mien-phi-hathanhhome", "/xay-nha-tron-goi-tai-ha-dong-khao-sat-tu-van-mien-phi-hathanhhome"],
+  ["/du-an/xay-nha-tron-goi-tai-nam-dinh-cap-nhat-bao-gia-moi-nhat-2026", "/xay-nha-tron-goi-tai-nam-dinh-cap-nhat-bao-gia-moi-nhat-2026"],
+  ["/du-an/xay-nha-tron-goi-hai-ba-trung", "/du-an/xay-nha-tron-goi-hai-ba-trung-hathanhhome"],
+  ["/90%20-mau-biet-thu-hien-dai-dep-2026", "/mau-thiet-ke-kien-truc/90-mau-biet-thu-hien-dai-dep-2026"],
+  ["/du-an/xay-nha-tron-goi-chuong-my-cap-nhap-bao-gia-moi-nhat", "/xay-nha-tron-goi-chuong-my-cap-nhap-bao-gia-moi-nhat"],
+  ["/99-%20-mau-nha-mai-nhat-dep-2026", "/mau-thiet-ke-kien-truc/99-mau-nha-mai-nhat-dep-2026"],
+];
+for (const [rawPath, target] of gscRedirects) {
+  assert.equal(getLegacyRedirectTarget(decodeURIComponent(rawPath)), target);
+}
 assert.equal(getLegacyRedirectTarget("/xay-nha-tron-goi-random"), undefined);
+assert.equal(getLegacyRedirectTarget("/du-an/xay-nha-tron-goi-long-bien-ha-noi-random"), undefined);
+assert.equal(getLegacyRedirectTarget("/70-other-page"), undefined);
 for (const path of [
   "/bao-gia-sua-chua-nha-ha-noi",
   "/sua-chua-cua-hang-tai-ha-noi-chuyen-nghiep",
